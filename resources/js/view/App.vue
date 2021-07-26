53389245928089af83b6a1ded59cd7e0b21dcaee
AbisaiSan <abisaiweb7@gmail.com>
2021-07-26 14:11:36 -0300

    First commit

---
+++ resources/js/view/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <router-link :to="{name: 'home'}">Home</router-link>
+        <router-link :to="{name: 'hello'}">Hello</router-link>
+        <router-link :to="{name: 'example'}">Example</router-link>
+
+        <div class="container">
+            <p>LaraVue SPA - Laravel + Vue</p>
+            <router-view></router-view>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ resources/js/view/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h2>Componente da page Home</h2>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
